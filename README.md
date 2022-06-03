ae9b7a58873bb9e4e67a0f4755e3114dd483a9db
Macedo <gemc@gft.com>
2022-06-02 22:45:03 -0300

    upload do README

---
+++ README.md
@@ -0,0 +1,20 @@
+# Santander Dev Week  :bank:
+
+   
+
+### Construção de uma API REST que gera extratos bancários do Santander.
+
+
+
+Aplicações utilizadas :gear:
+
+- Java 11 LTS
+- Spring Boot
+- Gerenciador de Dependências: Maven
+- IDE: Intellij community
+- Database: MariaDB
+
+<img src= "img/image.png">
+
+
+
--- bankline-api/src/main/resources/import.sql
+++ bankline-api/src/main/resources/import.sql
@@ -1,1 +1,3 @@
-INSERT INTO tb_correntista(account_number, account_balance, cpf, name) values(3366444,'125.35','07981367488','Gustavo Macedo')
+INSERT INTO tb_correntista(account_number, account_balance, cpf, name) VALUES(3366444,'125.35','07981367488','Gustavo Macedo')
+INSERT INTO tb_movimentacao(description, amount, type, idAccount) VALUES('Tranferência recebida',50,'REVENUE',1)
+INSERT INTO tb_movimentacao(description, amount, type, idAccount) VALUES('Saque realizado',30,'EXPENDITURE',1)
